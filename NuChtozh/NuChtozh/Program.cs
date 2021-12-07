56d91c67e8c72ec068d302bc4d9e6f285194d766
Andi74R <79770410+Andi74R@users.noreply.github.com>
2021-12-07 11:32:38 +0500

    Add files via upload
    add

---
+++ NuChtozh/NuChtozh/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NuChtozh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            //Console.WriteLine("Начинаем?");
+            //Console.ReadKey();
+            //Console.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                a = A(a);
+            }
+            Console.ReadKey();
+        }
+
+        static double A(double a=1)
+        {
+            double b = 3, c = 4;
+            a = (Math.Pow((b * c) / 2, c)* a * 10 - c)/b/2.03/1.4/1.02/3.2/2.11/5.223;
+            Console.WriteLine("\n" + a);
+            return a;
+        }
+    }
+}
